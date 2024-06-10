8a4dc5fa8d346e0590ca0ba57df0b5c7f5782ce1
Doaa-salem <doaa2essam@gmail.com>
2024-06-10 01:40:51 +0100

    update gaps

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -25,7 +25,7 @@ const Detail = ({ exerciseDetail }) => {
 
   return (
     <Stack
-      gap='60px'
+      gap='50px'
       sx={{ flexDirection: { lg: "row" }, p: "20px", alignItems: "center" }}>
       <img src={gifUrl} alt={name} className='detail-image' loadind='lazy' />
       <Stack sx={{ gap: { lg: "35px", xs: "20px" } }}>
